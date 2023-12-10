4edd55292068edebd9fbbd3e4982f84f3331a945
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2023-12-10 15:19:22 +0100

    Better test

--- test/test_micro_caml.ml
+++ test/test_micro_caml.ml
@@ -4,53 +4,59 @@ module Float_Graph = Make_Graph (Float_t)
 
 let%expect_test "Test add with constant" =
   let open Float_Graph in
-  let add_const () = build (fun () ->
+  let add_const () =
+    build (fun () ->
       let a = value_of 1.0 in
-      let comp = (const 2.0) + a in
+      let comp = const 2.0 + a in
       backward comp;
-      print_float @@ realize (grad a)
-    );
-    [%expect{| 1. |}]
+      print_float @@ realize (grad a));
+    [%expect {| 1. |}]
   in
-  let add_const_var () = build (fun () ->
+  let add_const_var () =
+    build (fun () ->
       let a = value_of 1.0 in
       let b = value_of 2.0 in
-      let comp = a + b + (const 2.0) in
+      let comp = a + b + const 2.0 in
       backward comp;
       print_float @@ realize (grad a);
-      print_float @@ realize (grad b)
-    );
-    [%expect{| 1.1. |}]
+      print_float @@ realize (grad b));
+    [%expect {| 1.1. |}]
   in
   add_const ();
   add_const_var ()
+;;
 
 let%expect_test "Test mul constant" =
   let open Float_Graph in
   build (fun () ->
-      let a = value_of 2.0 in
-      let comp1 = a * (const 2.0) in
-      let comp2 = a * (const 1.0) in
-      backward comp1;
-      print_float @@ realize (grad a);
-      (* This will reset the gradients. Do we want that?*)
-      backward comp2;
-      print_float @@ realize (grad a)
-    );
-  [%expect{| 2.1. |}]
+    let a = value_of 2.0 in
+    let comp1 = a * const 2.0 in
+    let comp2 = a * const 1.0 in
+    backward comp1;
+    print_float @@ realize (grad a);
+    (* This will reset the gradients. Do we want that?*)
+    backward comp2;
+    print_float @@ realize (grad a));
+  [%expect {| 2.1. |}]
+;;
 
 let compound_expr () =
   let open Float_Graph in
   build (fun () ->
     let a = value_of 4.0 in
-    let comp = a * a * a in
+    let b = value_of 3.0 in
+    let c = value_of 2.0 in
+    let comp = (a * (a + c) * a) + (const 2.0 * a * b) in
     backward comp;
     let ga = grad a in
-    (* print_endline @@ show ga; *)
-    print_float @@ realize ga)
+    let gb = grad b in
+    let gc = grad c in
+    print_float @@ realize ga;
+    print_float @@ realize gb;
+    print_float @@ realize gc)
 ;;
 
 let%expect_test "Test compound expression" =
   compound_expr ();
-  [%expect {|48.|}]
+  [%expect {|70.8.16.|}]
 ;;
